f7431e867d42c24e25192a332394ff3b5cf2f6ab
John Mark Rocela <me@iamjamoy.com>
2010-11-30 09:16:30 +0800

    navigation working to n-depth

--- core/scripts/concerto.js
+++ core/scripts/concerto.js
@@ -1,13 +1,13 @@
 jQuery(function($){
 	$('#access .menu li').hover(function() {
 		if ($(this).children('.children').length) {
-			$(this).find('.children').slideDown(100);
+			$(this).find('.children:eq(0)').slideDown(100);
 			$(this).find('a:eq(0)').addClass('active');
 		}
 	}, function() {
 		if ($(this).children('.children').length) {
 			var storethis = this;
-			$(this).find('.children').slideUp(100, function(){
+			$(this).find('.children:eq(0)').slideUp(100, function(){
 				$(storethis).find('a:eq(0)').removeClass('active');
 			});
 		}
