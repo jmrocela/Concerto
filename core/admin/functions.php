c441a19fe261a91f95d1fb27601ba9157101c625
John Mark Rocela <me@iamjamoy.com>
2011-01-08 21:58:57 +0800

    Added Export and Import methods for Concerto configurations

--- core/admin/functions.php
+++ core/admin/functions.php
@@ -1,8 +1,8 @@
 <?php
 
-function createOptionsFile($context = 'default', $what = -1){
+function exportConcertoOptions($what = null, $stage = 'default'){
 	// Should add a hook somewhere to support custom option exports
-	if (!is_array($what) && $what != -1) {
+	if (!is_array($what) && $what != null) {
 		if ($exploded = @explode(',', $what)) {
 			$what = array_map('trim', $exploded);
 		} else {
@@ -16,29 +16,40 @@ function createOptionsFile($context = 'default', $what = -1){
 	$general = $design = array();
 
 	foreach ($alloptions as $key => $value) {
-		$pos = strpos($key, 'concerto_' . $context . '_general_');
+		$pos = strpos($key, 'concerto_' . $stage . '_general_');
 		if ($pos !== false ) {
 			$general[$key] = $value;
 		}
-		$pos = strpos($key, 'concerto_' . $context . '_design_');
+		$pos = strpos($key, 'concerto_' . $stage . '_design_');
 		if ($pos !== false ) {
 			$design[$key] = $value;
 		}
 	}
 	
 	$content = array();
-	if ($what == -1) {
+	if ($what == null) {
 		$content['general'] = $general;
 		$content['design'] = $design;
+		$what = array('general','design');
+		$filename = '';
 	} else {
-		if (in_array('general', $what)) $content['general'] = $general;
-		if (in_array('design', $what)) $content['design'] = $design;
+		if (in_array('general', $what)) {
+			$content['general'] = $general;
+			$filename = '.general';
+		}
+		if (in_array('design', $what)) {
+			$content['design'] = $design;
+			$filename = '.design';
+		}
+		if (in_array('design', $what) && in_array('general', $what)) {
+			$filename = '';
+		}
 	}
 
 	$jsondcontent = json_encode($content);
 	
 	// We prepare the file for download
-	$filename = 'concerto' . date('mdY') . '.json';
+	$filename = 'concerto' . $filename . date('mdY') . '.cfwconf';
 	header('Content-Type: application/force-download'); 
 	header('Content-Type: application/octet-stream');
 	header('Content-Disposition: attachment; filename="' . $filename . '"');
@@ -48,10 +59,34 @@ function createOptionsFile($context = 'default', $what = -1){
 	header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
 ?>
 // This is an exported option file for Concerto generated on <?php echo date('r') ."\n"; ?>
-// Stage: <?php echo $context . "\n"; ?>
+// Stage: <?php echo $stage . "\n"; ?>
 // Context: <?php echo implode(', ', $what) ."\n"; ?>
 <?php echo $jsondcontent; ?>
 <?php
+	die();
+}
+
+function importConcertoOptions($file = null, $context = null, $stage = 'default') {
+	if (is_readable($file)) {
+		if (!is_array($context)) {
+			if ($context != null) {
+				if ($exploded = @explode(',', $context)) {
+					$context = array_map('trim', $exploded);
+				} 
+			}
+		}
+		$content = explode("\n", file_get_contents($file), 4);
+		$options = json_decode($content[3]);
+		foreach ($options as $what => $hash) {
+			if ($context == null || in_array($what, (array) $context)) {
+				foreach ($hash as $key => $value) {
+					$key = preg_replace('/concerto_(.*)_' . $what . '/', 'concerto_' . $stage . '_' . $what, $key );
+					echo $key . "-" . $value . "<br/>";
+					update_option($key, $value);
+				}
+			}
+		}
+	}
 	die();
 }
 
