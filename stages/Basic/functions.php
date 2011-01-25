3123349a100a57c5a8a30eecd313b18045f3bc0d
John Mark Rocela <me@iamjamoy.com>
2011-01-25 12:30:24 +0800

    added default theme to package

---
+++ stages/Basic/functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * functions.php
+ *
+ * "Basic" - A Concerto Stage Theme
+ * 
+ * This theme serves as a base file for future stage development. You may extend this file through style.css
+ * and functions.php(this file). This file will not be modified when Concerto is updated.
+ */
+
+// Add your hooks here
+
+?>
