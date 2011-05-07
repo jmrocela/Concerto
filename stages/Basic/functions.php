461c42a30c65c8a0f009e7bcdc8567752833737f
jmrocela <me@iamjamoy.com>
2011-05-07 11:37:54 +0800

    Added sticky footer capability for basic stage

--- stages/Basic/functions.php
+++ stages/Basic/functions.php
@@ -24,5 +24,23 @@ function basic_banner() {
 }
 add_action('concerto_hook_header_content', 'basic_banner', 20);
 
-
+/**
+ * Sticky Footer
+ *
+ * script to run whenever a page is too short and the footer does not stick to the bottom
+ */
+function sticky_footer() {
+	?>
+	<script type="text/javascript">
+		jQuery(function($){
+			if($(document.body).height() < $(window).height()){
+				$('#footer').css({position:'absolute',top:( $(window).scrollTop() + $(window).height() - $("#footer").height() ) + "px",width:"100%"});
+			} else {
+				$('#footer').css({position: 'static'});
+			}   
+		});
+	</script>
+	<?php
+}
+add_action('concerto_hook_footer', 'sticky_footer');
 ?>
